5f66241779d8c21bb45a36125f71d279a7e8c970
Kelubai <calebware@gmail.com>
2018-03-18 20:05:58 -0600

    Added hard-coded search results, updated components accordingly.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,48 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Hard-coded search results
+    this.state = {
+  searchResults: [
+    {
+      name: 'Fractured',
+      artist: 'RED',
+      album: 'Gone'
+    },
+    {
+      name: 'E for Extinction',
+      artist: 'Thousand Foot Krutch',
+      album: 'Welcome to the Masquerade'
+    },
+    {
+      name: 'Patience',
+      artist: 'Demon Hunter',
+      album: 'Outlive'
+    }
+  ],
+  playlistName: 'Blood and Sweat',
+  playlistTracks: [
+    {
+      name: 'Fractured',
+      artist: 'RED',
+      album: 'Gone'
+    },
+    {
+      name: 'E for Extinction',
+      artist: 'Thousand Foot Krutch',
+      album: 'Welcome to the Masquerade'
+    },
+    {
+      name: 'Patience',
+      artist: 'Demon Hunter',
+      album: 'Outlive'
+    }
+  ]
+};
+  }
+
   render() {
     return (
       <div>
@@ -12,8 +54,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './Playlist.css';
+import React, { Component } from 'react';
 import TrackList from '../TrackList/TrackList';
+import './Playlist.css';
 
-class Playlist extends React.Component {
+class PlayList extends React.Component {
   render() {
     return (
       <div className="Playlist">
         <input defaultValue={'New Playlist'}/>
-        {/* <TrackList /> */}
+        <TrackList tracks={this.props.playlistTracks}/>
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
       </div>
     );
   }
 }
 
-export default Playlist;
+export default PlayList;
